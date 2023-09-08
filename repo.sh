376bf1b6b2400b72a9c0aa50d16f8a00f76fecac
Ankur Srivastava <ankursrivastava2070@gmail.com>
2023-09-08 13:07:31 +0530

    clipboard functionality added for security purpose along with browser open view added

--- repo.sh
+++ repo.sh
@@ -1,5 +1,5 @@
-git config --global user.name "Your name"
-git config --global user.email "Your email"
+git config --global user.name "<Your Name>"
+git config --global user.email "<Your Email>"
 echo "Enter reponame"
 read reponame
 mkdir ${reponame}
@@ -8,14 +8,20 @@ x=`echo "$reponame" | tr -s ' '`
 final=`echo "$x" | tr " " -`
 echo "Enter (true) for public or (false) for private"
 read scope
-curl -i -H "Authorization: token your-access-token" -d '{"name": "'$final'","public": "'$scope'"}' https://api.github.com/user/repos
+public_token="<Your_public_token>"
+username="<Your_Github_Username>"
+curl -i -H "Authorization: token $public_token" -d '{"name": "'$final'","public": "'$scope'"}' https://api.github.com/user/repos
 git init
 git checkout -b main
 echo "Enter commit message for README.md"
 read msg
 touch README.md
+echo "Starter Message" >> README.md
 git add README.md
 git commit -m "$(echo $msg)"
-git remote add origin https://github.com/username/$final.git
-echo "paste access token as password:- your-access-token"
+git remote add origin https://github.com/$username/$final.git
+echo -n "$public_token" | xclip -selection clipboard
+echo -e  "\nYour username is :  $username"
+echo -e  "\nFor password press ctrl+V and then enter\n"
 git push origin main
+wslview "https://github.com/$(echo $username)/$(echo $final)"
